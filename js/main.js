12a794b1341f311999fbd6594855e0e9ec8ced36
naatii <jcorber855@g.educaand.es>
2023-05-14 13:16:52 +0200

    fin

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const login = "../prueba.html"
+function sorpresa(){
+    const sorpresa = document.getElementById("sorpresa")
+    const botonSorpresa = document.getElementById("boton-sorpresa")
+    const footer = document.getElementById("footer")
+
+    footer.style.bottom = "-80em"
+    botonSorpresa.style.display = "none"
+    sorpresa.style.display = "flex"
+    sorpresa.style.flexDirection = "column"
+    sorpresa.style.justifyContent = "center"
+    sorpresa.style.alignContent = "center"
+    sorpresa.style.alignItems = "center"
+    sorpresa.style.height = "100%"
+    sorpresa.style.zIndex = "1000"
+    sorpresa.style.textAlign = "center"
+    sorpresa.style.backgroundColor = "rgba(0, 0, 0, .8)"
+    sorpresa.style.color = "white"
+    sorpresa.style.boxShadow = "0 0 20px violet, 0 0 20px violet, 0 0 20px violet, 0 0 20px violet, 0 0 20px violet"
+    sorpresa.style.fontSize = "2em"
+    sorpresa.style.width = "60%"
+    sorpresa.style.height = "100%"
+    sorpresa.style.padding = "1em 1em"
+    sorpresa.style.letterSpacing = ".2em"
+}
+function validar(){
+    const user = document.getElementById("nombre").value;
+    const constraseña = document.getElementById("contraseña").value;
+
+    if (user == "patita" && constraseña == "teamo"){
+        window.location.href = "../prueba.html"
+    }
+    else{
+        alert("Contraseña o usuario no es válido")
+    }
+}
